cd64bddc1c0e4874a11c58dc89d98c9ada6ee1dd
ricky9090 <unreal_star13@126.com>
2021-02-01 00:19:54 +0800

    format with intellij idea

--- src/JSqueak/MouseStatus.java
+++ src/JSqueak/MouseStatus.java
@@ -1,64 +1,58 @@
 package JSqueak;
 
+import javax.swing.event.MouseInputAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.event.MouseInputAdapter;
-
-class MouseStatus extends MouseInputAdapter 
-{
+class MouseStatus extends MouseInputAdapter {
     private final SqueakVM fSqueakVM;
-    
+
     int fX, fY;
     int fButtons;
-    
+
     private final static int RED = 4;
     private final static int YELLOW = 2;
     private final static int BLUE = 1;
-    
-    MouseStatus( SqueakVM squeakVM )
-    {
+
+    MouseStatus(SqueakVM squeakVM) {
         fSqueakVM = squeakVM;
     }
-    
-    private int mapButton(MouseEvent evt) 
-    {
-        switch (evt.getButton()) 
-        {
+
+    private int mapButton(MouseEvent evt) {
+        switch (evt.getButton()) {
             case MouseEvent.BUTTON1:
-                if (evt.isControlDown()) 
+                if (evt.isControlDown())
                     return YELLOW;
-                if (evt.isAltDown()) 
+                if (evt.isAltDown())
                     return BLUE;
                 return RED;
-            case MouseEvent.BUTTON2:    return BLUE;        // middle (frame menu)
-            case MouseEvent.BUTTON3:    return YELLOW;  // right (pane menu)
-            case MouseEvent.NOBUTTON:   return 0;
+            case MouseEvent.BUTTON2:
+                return BLUE;        // middle (frame menu)
+            case MouseEvent.BUTTON3:
+                return YELLOW;  // right (pane menu)
+            case MouseEvent.NOBUTTON:
+                return 0;
         }
-        throw new RuntimeException("unknown mouse button in event"); 
+        throw new RuntimeException("unknown mouse button in event");
     }
-    
-    public void mouseMoved(MouseEvent evt) 
-    {
+
+    public void mouseMoved(MouseEvent evt) {
         fX = evt.getX();
         fY = evt.getY();
-        fSqueakVM.wakeVM(); 
+        fSqueakVM.wakeVM();
     }
-    
-    public void mouseDragged(MouseEvent evt) 
-    {
-        fX= evt.getX();
-        fY= evt.getY();
-        fSqueakVM.wakeVM(); 
+
+    public void mouseDragged(MouseEvent evt) {
+        fX = evt.getX();
+        fY = evt.getY();
+        fSqueakVM.wakeVM();
     }
-    
-    public void mousePressed(MouseEvent evt) 
-    {
+
+    public void mousePressed(MouseEvent evt) {
         fButtons |= mapButton(evt);
-        fSqueakVM.wakeVM(); 
+        fSqueakVM.wakeVM();
     }
-    
-    public void mouseReleased(MouseEvent evt) 
-    {
+
+    public void mouseReleased(MouseEvent evt) {
         fButtons &= ~mapButton(evt);
         fSqueakVM.wakeVM();
     }
